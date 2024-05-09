fb134cd8603a577cc877c2cfb44d205d11d8f80e
macauy <maca.rabosto@gmail.com>
2024-05-09 15:30:48 -0300

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 4000;
+console.log(process.env.PORT);
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
