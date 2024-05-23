94c5e1fa68e34f7065f48d930bc2dcfebcfb1e9b
larizzamrl <larizzamrl@gmail.com>
2024-05-23 14:51:45 -0600

    instrucciones ch

--- app.js
+++ app.js
@@ -16,4 +16,24 @@ app.use('/api/v1/auth', AuthRoutes);
 app.use('/api/v1/users', UserRoutes);
 app.use('/api/v1/entities', EntityRoutes);
 
+//todo: Crear una ruta para emplooyeeDocuments
+
+//todo: Crear un archivo en routes que se llame employeeDocuments.routes.js
+  //? EMPLOYEEDOCUMENTS.ROUTES:
+    //todo: Hacer SOLO la ruta de GET pero sin authentication
+
+//todo: Crear un archivo en controllers que se llame employeeDocuments.controller.js
+  //? EMPLOYEEDOCUMENTS.CONTROLLER:
+    //todo: Hacer SOLO el controller de GET 
+
+//todo: Crear un archivo en service que se llame employeeDocuments.service.js
+  //? EMPLOYEEDOCUMENTS.SERVICE:
+    //todo: lo mismo (ve revisando los otros archivos para que te vayas guiando)
+
+//todo: Crear un archivo en repository que se llame employeeDocuments.repository.js
+  //? EMPLOYEEDOCUMENTS.REPOSITORY:
+    //! PENDIENTE (AVISAR CUANDO TEMRINES)
+
+//* Cuando termines has lo mismo pero con Employee (avisame cuando ya empieces esto)
+
 export default app;
