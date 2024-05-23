bf81f24183fc0bed9ad42d405c76cd999991f0fb
Sergio Terroso <sergio.terroso@email.com>
2024-05-22 19:22:56 -0600

    fixing: seed errors

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { randomUUID } from "node:crypto";
-import { hash } from "bcryptjs";
+import bcrypt from "bcryptjs";
 import { fakerES_MX } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
@@ -58,6 +57,32 @@ const TEST_AREAS_NAMES = [
   "Piso 1, Zona D",
   "Piso 1, Zona E",
   "Piso 2",
+];
+
+const EMPLOYEES_STATUS = [
+  "Alta",
+  "Descanso",
+  "Vacaciones",
+  "Incapacidad",
+  "Baja temporal",
+  "Baja permanente",
+];
+
+const EMPLOYEES_ID_DOCUMENT_TYPES = [
+  "INE",
+  "Pasaporte",
+  "DNI",
+  "Otro"
+];
+
+const EMPLOYEES_STUDIES = [
+  "Ninguno",
+  "Primaria",
+  "Secundaria",
+  "Medio",
+  "Licenciatura",
+  "Maestria o superior",
+  "No especificado"
 ];
 
 const MIN_AREAS_COUNT = 3;
@@ -144,13 +169,21 @@ const getRandomInt = (min, max) => {
   return min + Math.floor(Math.random() * range);
 };
 
+const getRandomElement = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  return array[randomIndex];
+};
+
 const main = async () => {
   try {
     // 1. Create Builtin Roles
+    console.info("📋 Creando Roles...");
     // 1.1. Huubie Superuser Builtin Role
+    console.info("\tCreando Rol de HuubieSuperuser");
     const huubieSuperUserRole = await prisma.role.create({
       data: {
-        name: "Huubie Superuser",
+        name: "HuubieSuperuser",
         is_builtin: true,
       },
       select: {
@@ -160,9 +193,10 @@ const main = async () => {
     });
 
     // 1.2. Entity Owner Builtin Role
+    console.info("\t✔️ Creando Rol de EntityOwner");
     const entityOwnerRole = await prisma.role.create({
       data: {
-        name: "Entity Owner",
+        name: "EntityOwner",
         is_builtin: true,
       },
       select: {
@@ -172,9 +206,10 @@ const main = async () => {
     });
 
     // 1.3. Entity Admin Builtin Role
+    console.info("\t✔️ Creando Rol de EntityAdmin");
     const entityAdminRole = await prisma.role.create({
       data: {
-        name: "Entity Admin",
+        name: "EntityAdmin",
         is_builtin: true,
       },
       select: {
@@ -184,9 +219,10 @@ const main = async () => {
     });
 
     // 1.4. Entity User Builtin Role
+    console.info("\t✔️ Creando Rol de EntityUser");
     const entityUserRole = await prisma.role.create({
       data: {
-        name: "Entity User",
+        name: "EntityUser",
         is_builtin: true,
       },
       select: {
@@ -195,11 +231,15 @@ const main = async () => {
       },
     });
 
-    // 2. Create Huubie Superuser Builtin user
+    // 2. Create Builtin Users
+    console.info("👥 Creando Usuarios...")
+
+    // 2.1 Create Huubie Superuser
+    console.info("\t✔️ Creando HuubieSuperuser");
     const builtinSuperUser = await prisma.user.create({
       data: {
         email: "huubie_superuser@test.com",
-        password: await hash("unbreakable", SALT_LENGTH),
+        password: await bcrypt.hash("unbreakable", SALT_LENGTH),
         first_name: "Superuser",
         last_name: "Huubie",
         role_id: huubieSuperUserRole.id,
@@ -213,6 +253,7 @@ const main = async () => {
     });
 
     // 3. Create Test Entity
+    console.info("🏢 Creando Entidad de prueba")
     const testEntity = await prisma.entity.create({
       data: {
         legal_name: "Entidad de Prueba, S.A. de C.V.",
@@ -226,14 +267,17 @@ const main = async () => {
     });
 
     // 4. Create Test Entity Users
+    console.info("👤 Creando usuarios de la Entidad de prueba...")
     // 4.1. Entity Owner
+    console.info("\t✔️ Creando Usuario EntityOwner (propietario o representante legal de la Entidad de prueba)");
     const testEntityOwner = await prisma.user.create({
       data: {
         email: "entity_owner@test.com",
-        password: await hash("owner1pass", SALT_LENGTH),
+        password: await bcrypt.hash("owner1pass", SALT_LENGTH),
         first_name: "Propietario",
         last_name: "Entidad de Prueba",
         entity_id: testEntity.id,
+        role_id: entityOwnerRole.id,
       },
       select: {
         id: true,
@@ -244,13 +288,15 @@ const main = async () => {
     });
 
     // 4.2. Entity Admin
+    console.info("\t✔️ Creando Usuario EntityAdmin (administrador de la Entidad de prueba)");
     const testEntityAdmin = await prisma.user.create({
       data: {
         email: "entity1_admin@test.com",
-        password: await hash("admin1pass", SALT_LENGTH),
+        password: await bcrypt.hash("admin1pass", SALT_LENGTH),
         first_name: "Admin",
         last_name: "Entidad de Prueba",
         entity_id: testEntity.id,
+        role_id: entityAdminRole.id,
       },
       select: {
         id: true,
@@ -260,8 +306,9 @@ const main = async () => {
       },
     });
 
-    const positions = [];
+    const branchPositions = [];
     // 5. Create Test Entity Positions
+    console.info("🕴️ Creando Puestos para la Entidad de prueba");
     TEST_POSITIONS.forEach(async (position) => {
       const newPosition = await prisma.position.create({
         data: {
@@ -275,10 +322,12 @@ const main = async () => {
         },
       });
 
-      positions.push(newPosition);
+      branchPositions.push(newPosition);
     });
 
     // 6. Create Test Entity Branches
+    const entityBranches = [];
+    console.log("🏤 Creando Sucursales para la Entidad de prueba");
     for (let i = 0; i < BRANCHES_COUNT; i++) {
       // 6.1. Create a new branch
       const newBranch = await prisma.branch.create({
@@ -292,11 +341,16 @@ const main = async () => {
           name: true,
         },
       });
+      entityBranches.push(newBranch);
+      console.log(`\tCreada la Sucursal '${newBranch.name}'`);
 
       // 6.2. Create Areas for the new Branch
+      console.info(`\t\t🛠️ Creando Areas para la Sucursal '${newBranch.name}'`);
+      const branchAreas = [];
       const areasCount = getRandomInt(MIN_AREAS_COUNT, MAX_AREAS_COUNT);
 
       // 6.2.1 Always create an Administration area per branch.
+      console.info("\t\t\t✔️ Administracion");
       const newBranchAdministrationArea = await prisma.area.create({
         data: {
           name: "Administracion",
@@ -307,8 +361,10 @@ const main = async () => {
           name: true
         }
       });
+      branchAreas.push(newBranchAdministrationArea);
 
       // 6.2.2 Create non-administrative areas
+      console.info("\t\t\t✔️ Areas no administrativas")
       const areasCreated = ["Administracion"];
       for (let j = 0; j < areasCount; j++) {
         const areasLeft = TEST_AREAS_NAMES.filter(
@@ -327,14 +383,16 @@ const main = async () => {
           },
         });
         areasCreated.push(newArea.name);
+        branchAreas.push(newArea);
       }
 
       // 6.3 Create Shifts for the new Branch
-      const newBranchShifts = [];
+      console.info(`\t\t🕑 Creando Turno(s) para la Sucursal '${newBranch.name}'`)
+      const branchShifts = [];
       const randomShiftsType = getRandomShiftTypeIndex();
       switch (randomShiftsType) {
         case "SINGLE_SHIFT_MORNINGS":
-          newBranchShifts.push(
+          branchShifts.push(
             await prisma.shift.create({
               data: {
                 name: "Unico",
@@ -353,7 +411,7 @@ const main = async () => {
           );
           break;
         case "SINGLE_SHIFT_NIGHT":
-          newBranchShifts.push(
+          branchShifts.push(
             await prisma.shift.create({
               data: {
                 name: "Unico",
@@ -388,7 +446,7 @@ const main = async () => {
                 hours: true,
               },
             });
-            newBranchShifts.push(newShift);
+            branchShifts.push(newShift);
           });
           break;
         case "DOUBLE_SHIFTS_EVENING":
@@ -408,7 +466,7 @@ const main = async () => {
                 hours: true,
               },
             });
-            newBranchShifts.push(newShift);
+            branchShifts.push(newShift);
           });
           break;
         case "TRIPLE_SHIFTS":
@@ -428,7 +486,7 @@ const main = async () => {
                 hours: true,
               },
             });
-            newBranchShifts.push(newShift);
+            branchShifts.push(newShift);
           });
           break;
         default:
@@ -436,20 +494,32 @@ const main = async () => {
       }
 
       // 6.4 Create Employees for the new Branch
-      const employeesCount = getRandomInt(
-        MIN_EMPLOYEES_COUNT_BY_BRANCH,
-        MAX_EMPLOYEES_COUNT_BY_BRANCH
-      );
-
+      console.info(`\t\t👷 Creando Empleados para la sucursal '${newBranch.name}'`)
       const managersCount = getRandomInt(
         MIN_MANAGERS_COUNT,
         Math.max(MIN_MANAGERS_COUNT, Math.floor(employeesCount * 0.1))
       );
 
+      const employeesCount = getRandomInt(
+        MIN_EMPLOYEES_COUNT_BY_BRANCH,
+        MAX_EMPLOYEES_COUNT_BY_BRANCH
+      );
+
       // 6.4.1 Create Managers
+      console.info("\t\t\t✔️ Gerentes");
       const branchManagers = [];
       const managerPositionsTaken = [];
-      for (let j = 0; j < managersCount; j++) {
+      const testManagerPositions = TEST_POSITIONS.filter(
+        (position) => position.is_manager
+      );
+      const managerPositionsAvailable = branchPositions.filter((position) =>
+        testManagerPositions.some(
+          (element) =>
+            element.name === position.title &&
+            !managerPositionsTaken.includes(position.title)
+        )
+      );
+    for (let j = 0; j < managersCount; j++) {
         const newManagerGender = fakerES_MX.person.sexType();
         const newManagerFirstName =
           fakerES_MX.person.firstName(newManagerGender);
@@ -457,20 +527,10 @@ const main = async () => {
         const newManagerEmail = fakerES_MX.internet.email({
           firstName: newManagerFirstName,
           lastName: newManagerLastName,
-          provider: "huubie.dev",
+          provider: "huubie.dev.test",
         });
-        const testManagerPositions = TEST_POSITIONS.filter(
-          (position) => position.is_manager
-        );
-        const managerPositionsAvailable = positions.filter((position) =>
-          testManagerPositions.some(
-            (element) =>
-              element.name === position.title &&
-              !managerPositionsTaken.includes(position.title)
-          )
-        );
         const newManagerPositionIndex = Math.floor(Math.random() * managerPositionsAvailable.length);
-        const newManagerPosition = managerPositionsAvailable[newManagerPositionIndex];
+        const newManagerPosition = managerPositionsAvailable.splice(newManagerPositionIndex, 1)[0];
         managerPositionsTaken.push(newManagerPosition.title);
         const newManager = await prisma.employee.create({
           data: {
@@ -480,12 +540,20 @@ const main = async () => {
             last_name: newManagerLastName,
             gender: newManagerGender === "female" ? "femenino" : "masculino",
             area_id: newBranchAdministrationArea.id,
-            shift_id: newBranchShifts[0].id,
+            shift_id: branchShifts[0].id,
             is_manager: true,
             telephone_number: fakerES_MX.helpers.fromRegExp('\+52 \([0-9]{3}\) [0-9]{3}\-[0-9]{4}'),
             position_id: newManagerPosition.id,
             tax_id: fakerES_MX.helpers.fromRegExp('[A-Z]{4}[0-9]{6}[A-Z0-9]{3}'),
+            date_of_hire: fakerES_MX.date.past({ years: 10 }),
             citizen_id_number: fakerES_MX.helpers.fromRegExp('[A-Z]{4}[0-9]{6}[HM]{1}[A-Z]{2}[A-Z]{3}[A-Z0-9]{2}'),
+            date_of_birth: fakerES_MX.date.birthdate({ min: 18, max: 54, mode: 'age' }),
+            status: getRandomElement(EMPLOYEES_STATUS),
+            study: getRandomElement(EMPLOYEES_STUDIES),
+            id_document_type: getRandomElement(EMPLOYEES_ID_DOCUMENT_TYPES),
+            id_document_number: fakerES_MX.helpers.fromRegExp('[A-Z]{3}\-[0-9]{3}\-[0-9]{3}\-[A-Z0-9]{4}'),
+            ssn: fakerES_MX.string.numeric(11),
+            daily_wage: fakerES_MX.number.int({ min: 100000, max: 400000 }),
           },
           select: {
             id: true,
@@ -499,9 +567,81 @@ const main = async () => {
       }
 
       // 6.4.2 Create Non-managers
-      for (let j = 0; j < employeesCount; j++) {}
+      console.info("\t\t\t✔️ Puestos no gerenciales");
+      const branchEmployees = [];
+      for (let j = 0; j < employeesCount; j++) {
+        const newEmployeeGender = fakerES_MX.person.sexType();
+        const newEmployeeFirstName = fakerES_MX.person.firstName(newEmployeeGender);
+        const newEmployeeLastName = fakerES_MX.person.lastName(newEmployeeGender);
+        const newEmployeeEmail = fakerES_MX.internet.email({
+          firstName: newEmployeeFirstName,
+          lastName: newEmployeeLastName,
+          provider: "huubie.dev.test"
+        });
+        const newEmployeeArea = getRandomElement(branchAreas.filter((area) => area.name !== "Administration"));
+        const newEmployeeShift = getRandomElement(branchShifts);
+        const newEmployeeManager = getRandomElement(branchManagers);
+        const newEmployeePosition = getRandomElement(branchPositions);
+        const newEmployee = await prisma.employee.create({
+          data: {
+            email: newEmployeeEmail,
+            first_name: newEmployeeFirstName,
+            last_name: newEmployeeLastName,
+            gender: newEmployeeGender === "female" ? "femenino" : "masculino",
+            area_id: newEmployeeArea.id,
+            shift_id: newEmployeeShift.id,
+            is_manager: false,
+            manager_id: newEmployeeManager.id,
+            telephone_number: fakerES_MX.helpers.fromRegExp('\+52 \([0-9]{3}\) [0-9]{3}\-[0-9]{4}'),
+            position_id: newEmployeePosition.id,
+            tax_id: fakerES_MX.helpers.fromRegExp('[A-Z]{4}[0-9]{6}[A-Z0-9]{3}'),
+            date_of_hire: fakerES_MX.date.past({ years: 8 }),
+            citizen_id_number: fakerES_MX.helpers.fromRegExp('[A-Z]{4}[0-9]{6}[HM]{1}[A-Z]{2}[A-Z]{3}[A-Z0-9]{2}'),
+            date_of_birth: fakerES_MX.date.birthdate({ min: 18, max: 36, mode: 'age' }),
+            status: getRandomElement(EMPLOYEES_STATUS),
+            study: getRandomElement(EMPLOYEES_STUDIES),
+            id_document_type: getRandomElement(EMPLOYEES_ID_DOCUMENT_TYPES),
+            id_document_number: fakerES_MX.helpers.fromRegExp('[A-Z]{3}\-[0-9]{3}\-[0-9]{3}\-[A-Z0-9]{4}'),
+            ssn: fakerES_MX.string.numeric(11),
+            daily_wage: fakerES_MX.number.int({ min: 25000, max: 80000 }),
+          },
+          select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true
+          }
+        });
+
+        branchEmployees.push(newEmployee);
+      }
+
+      // 6.5 Assign users to Branch Employees
+      console.info(`\t\tAsignando usuarios a los Empleados de la Sucursal '${newBranch.name}'`);
+      branchEmployees.forEach(async (employee) => {
+        newEntityUser = await prisma.user.create({
+          data: {
+            email: employee.email,
+            password: await bcrypt.hash(employee.tax_id, SALT_LENGTH),
+            first_name: employee.first_name,
+            last_name: employee.last_name,
+            role_id: entityUserRole.id,
+            entity_id: testEntity.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            first_name: true,
+            last_name: true,
+          }
+        });
+      });
     }
-  } catch (err) {}
+    console.info("👌 Se han creado los datos semilla en la base de datos.");
+  } catch (err) {
+    console.error("❗ Ocurrio un error en la creacion de los datos semilla en la base de datos.");
+    console.error(err);
+  }
 };
 
 try {
